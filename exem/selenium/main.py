0e6c844358be92f248dfec5fe1f9f66cc71213b0
Sumin Jo <suminjo725@gmail.com>
2023-08-04 20:06:32 +0900

    selenium setting

---
+++ exem/selenium/main.py
@@ -0,0 +1,22 @@
+# 외우는거 아님. 그냥 필요할 때 복붙
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+# 크롬 드라이버 자동 업데이트
+from webdriver_manager.chrome import ChromeDriverManager
+
+#브라우저 꺼짐 방지 
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# 불필요한 에러 메시지 없애기
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+browser = webdriver.Chrome(options=chrome_options)
+
+browser.get('http://naver.com')
+time.sleep(1)
+browser.close() 
